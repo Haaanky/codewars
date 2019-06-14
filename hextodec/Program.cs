ce01431ca709f2141225ed1466b0a9066f33ba2e
Haaanky <Haaanky@gmail.com>
2019-06-14 18:43:45 +0200

    1233

---
+++ hextodec/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace hextodec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kata.HexToDec("1");
+            Kata.HexToDec("a");
+            Kata.HexToDec("10");
+            Kata.HexToDec("FF");
+            Console.WriteLine(Kata.HexToDec("-C"));
+        }
+    }
+    public class Kata
+    {
+        public static int HexToDec(string hexString)
+        {
+            return hexString.Contains('-') ? -1 * int.Parse(hexString.Substring(1), NumberStyles.HexNumber) : int.Parse(hexString, NumberStyles.HexNumber);
+        }
+    }
+}
