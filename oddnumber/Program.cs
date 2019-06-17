d6d3c3dc21fd0ae89c71842d4b2cbc64c66dc97c
Henrik Norlin Frisemo <haaanky@gmail.com>
2019-06-17 09:29:51 +0200

    oddnumber complete

---
+++ oddnumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace oddnumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.find_it(new int[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 }));
+        }
+    }
+    class Kata
+    {
+        public static int find_it(int[] seq)
+        {
+            foreach (var item in seq.GroupBy(x => x))
+            {
+                if (item.Count() % 2 != 0)
+                    return item.Key;
+            }
+            return -1;
+        }
+
+    }
+}
