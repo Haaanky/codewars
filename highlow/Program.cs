06709a8657176565437220add0765a5f8dba72f3
Henrik Norlin Frisemo <haaanky@gmail.com>
2019-06-17 09:42:49 +0200

    highlow completed

---
+++ highlow/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace highlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kata.HighAndLow("1 2 3 4 5");  // return "5 1"
+            Kata.HighAndLow("1 2 -3 4 5"); // return "5 -3"
+            Kata.HighAndLow("1 9 3 4 -5"); // return "9 -5"
+        }
+    }
+    public static class Kata
+    {
+        public static string HighAndLow(string numbers)
+        {
+            return $"{numbers.Split().Max()} {numbers.Split().Min(x => int.Parse(x))}";
+        }
+    }
+}
