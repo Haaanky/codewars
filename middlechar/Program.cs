04c3eb8e486dd08a8fff4a8009f3d595ef7b7a41
Henrik Norlin Frisemo <haaanky@gmail.com>
2019-06-17 11:09:00 +0200

    getmiddle completed

---
+++ middlechar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace middlechar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.GetMiddle("A"));
+        }
+    }
+    public class Kata
+    {
+        public static string GetMiddle(string s)
+        {
+            return s.Length % 2 == 0 ? s.Substring((int)Math.Round((s.Length / 2.0), MidpointRounding.AwayFromZero) - 1, 2) : s[s.Length / 2].ToString();
+        }
+    }
+}
