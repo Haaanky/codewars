4982225550c03a23570986922da230d9155c27e8
Henrik Norlin Frisemo <haaanky@gmail.com>
2019-06-17 11:29:41 +0200

    pairtyoutlier completed

---
+++ parityoutlier/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace parityoutlier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Find(new int[] { 1, -3, 9, -10, 3, 19 }));
+        }
+    }
+    public class Kata
+    {
+        public static int Find(int[] integers)
+        {
+            return integers.GroupBy(x => x % 2 == 0).Where(x => x.Count() == 1).Single().Single();
+        }
+    }
+}
