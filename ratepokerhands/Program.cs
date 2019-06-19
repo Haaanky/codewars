78424205f96a8f1ba85196df5453bc4612097672
Henrik Norlin Frisemo <haaanky@gmail.com>
2019-06-19 12:32:29 +0200

    working on pokerhand

--- ratepokerhands/Program.cs
+++ ratepokerhands/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ratepokerhands
 {
@@ -6,7 +9,7 @@ namespace ratepokerhands
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(new PokerHand("KS 2H 5C JD TD").CompareWith(new PokerHand("opponent")));
         }
     }
     public enum Result
@@ -27,7 +30,58 @@ namespace ratepokerhands
 
         public Result CompareWith(PokerHand hand)
         {
-            return Result.Tie;
+            //Result result;
+
+            //if (Hand > hand)
+            //{
+            //    result = Result.Win;
+            //}
+            //else if(Hand < hand)
+            //{
+            //    result = Result.Loss;
+            //}
+            //else
+            //{
+            //    result = Result.Tie;
+            //}
+
+
+            switch (CompareHands(Hand, hand.Hand))
+            {
+                case Result.Win:
+                    return Result.Win;
+                case Result.Loss:
+                    return Result.Loss;
+                default:
+                    return Result.Tie;
+            }
+        }
+
+        private Result CompareHands(string hand1, string hand2)
+        {
+            var firstHand = hand1.Split();
+            var secondHand = hand2.Split();
+
+            var tmp = new string[][] { firstHand, secondHand };
+
+            for (int i = 0; i < 5; i++)
+            {
+                foreach (var item in tmp)
+                {
+                    switch (item[i][0])
+                    {
+                        case 'A': item[i] = "14" + item[i].Substring(1); break;
+                        case 'K': item[i] = "13" + item[i].Substring(1); break;
+                        case 'Q': item[i] = "12" + item[i].Substring(1); break;
+                        case 'J': item[i] = "11" + item[i].Substring(1); break;
+                        case 'T': item[i] = "10" + item[i].Substring(1); break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            return Result.Loss;
         }
     }
 }
